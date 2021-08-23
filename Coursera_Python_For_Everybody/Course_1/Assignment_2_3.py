695d0c509c25ce3f66d18b7525c7565e0c103bbd
tanu841 <39293755+tanu841@users.noreply.github.com>
2021-08-23 11:07:14 +0530

    Create Assignment_2_3.py

---
+++ Coursera_Python_For_Everybody/Course_1/Assignment_2_3.py
@@ -0,0 +1,11 @@
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+h = float(hrs)
+r = float(rate)
+pay = r*h
+print("Pay: " , pay)
+
+#or
+#hrs = float(input("Enter Hours:"))
+#rate = float(input("Enter Rate:"))
+#print("Pay: " , hrs*rate)
