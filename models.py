75cb0983a9351ab00e392a16c510fbbaa46152e7
Tanu841 <tanusingha841@gmail.com>
2025-01-24 13:59:18 +0530

    Faxcst API database set up

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from database import Base
 
 class Books(Base):
-    __tablename__="books"a
+    __tablename__="books"
     id = Column(Integer,primary_key=True, index=True)
     title=Column(String)
     author=Column(String)
