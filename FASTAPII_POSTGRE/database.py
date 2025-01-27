04289e53a814c6b15a8e5be735f2e789827357ba
Tanu841 <tanusingha841@gmail.com>
2025-01-27 22:12:32 +0530

    POSTGRE

--- FASTAPII_POSTGRE/database.py
+++ FASTAPII_POSTGRE/database.py
@@ -2,3 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
+URL_DATABASE='postgresql://postgres:password@localhost:5432/QuizApplicationYT'
+#URL_DATABASE='postgresql://postgres:password@localhost:5432/QuizApplicationYT'
+
+engine=create_engine(URL_DATABASE)
+
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+Base=declarative_base()
+#pip install psycopg2-binary
+#cd FASTAPII_POSTGRE
+#uvicorn main:app --reload
