76ef482e04719156737ae042e8da276005a25e92
Tanu <39293755+tanu841@users.noreply.github.com>
2025-01-30 11:36:04 +0530

    Delete FASTAPI_MONGO directory

--- FASTAPI_MONGO/configts/database.py
+++
@@ -1,4 +0,0 @@
-from pymongo import MongoClient
-client=MongoClient("mongodb+srv://admin:admin1234@cluster0.xrmp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db=client.todo_db
-collection_name=db["todo_collection"]
