8de72bfba748a16744fd5e5d85c8c87ee5287928
Tanu841 <tanusingha841@gmail.com>
2025-02-13 20:03:30 +0530

    if else

---
+++ FastAPI_codes/chatbot/basics/if_else.py
@@ -0,0 +1,14 @@
+grade =73
+print(grade)
+print(type(grade))
+
+if grade >=90 and grade<=100:
+    print("A")
+elif grade >=80 and grade<90:
+    print("B")
+elif grade >=70 and grade<80:
+    print("C")
+elif grade >=60 and grade<70:
+    print("D")
+else:
+    print("F")
