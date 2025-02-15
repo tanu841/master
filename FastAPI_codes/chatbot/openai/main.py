a76d7e2c2b249d5529f9af94ecd5af2dc49e2895
Tanu841 <tanusingha841@gmail.com>
2025-02-15 18:03:35 +0530

    openai

--- FastAPI_codes/chatbot/openai/main.py
+++ FastAPI_codes/chatbot/openai/main.py
@@ -14,25 +14,28 @@ openai=OpenAI(
 async def chat_page(request: Request):
     return templates.TemplateResponse("chat.html",{"request":request})
 
+chat_log=[{
+    'role': 'system',
+    'content': 'You are a python tutoe AI,completely dedicated to teach users how to learn \
+               Python from scratch. Please provide clear instructions on the Python concepts,\
+               best practices, and coding examples to help users learn Python effectively.'
+}]
+chat_responses=[]
 
+@app.post("/",response_class=HTMLResponse)
+async def chat(request:Request,user_input:Annotated[str,Form()]):
+    chat_log.append({'role':'user','content':user_input})
+    chat_responses.append(user_input)
 
-response = openai.chat.completions.create(
+    response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
-    messages=[{
-        "role": "system",
-        'content': "You are a helpful assistant."
-    },{
-        "role": "assistant",
-        'content': "The Spurs won the 2005 NBA championship"
-    }
-    ,
-    {
-        "role": "user",
-        'content': "Who won NBA championship at 2005?"
-    }]
-)
-
-print(response)
+    messages=chat_log,
+    temperature=0.6
+    )
+    bot_response = response['choices'][0]['message']['content']
+    chat_log.append({'role':'assistant','content':bot_response})
+    chat_responses.append(bot_response)
+    return templates.TemplateResponse("home.html",{"request":request, " chat_responses":chat_responses})
 
 # Output
 #% python3 main.py
